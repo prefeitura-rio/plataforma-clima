e95a8889ee8b0557e477ee5773cc21ce54caefad
Lucas Tavares <lucastavarestt@gmail.com>
2024-09-04 08:34:20 -0300

    fix: satelite view

--- src/app/(demo)/satelite/[sateliteView]/page.tsx
+++ src/app/(demo)/satelite/[sateliteView]/page.tsx
@@ -1,6 +1,4 @@
 import { ContentLayout } from "@/components/admin-panel/content-layout";
-import { Breadcrumb, BreadcrumbItem, BreadcrumbLink, BreadcrumbList, BreadcrumbPage, BreadcrumbSeparator } from "@/components/ui/breadcrumb";
-import Link from "next/link";
 import SateliteContent from "../satelite-content";
 
 interface SateliteViewProps {
@@ -9,18 +7,28 @@ interface SateliteViewProps {
   };
 }
 
-
+// Dynamic page component
 export default async function SateliteView({ params }: SateliteViewProps) {
   const response = await fetch("https://rnc.dados.rio/GetData", {
-    cache: 'no-store'
+    cache: 'no-store', // No caching, fresh data on every request
   });
   const apiResponse = await response.json();
 
   return (
-
     <ContentLayout title="Satélite">
       <SateliteContent sateliteView={params.sateliteView} data={apiResponse} />
     </ContentLayout>
+  );
+}
 
-  )
+// Generate static paths for predefined satellite views
+export async function generateStaticParams() {
+  // List of static satellite views
+  const satelliteViews = ['CP', 'KI', 'LI', 'SI', 'TT'];
+
+  return satelliteViews.map((view) => ({
+    params: {
+      sateliteView: view,
+    },
+  }));
 }
