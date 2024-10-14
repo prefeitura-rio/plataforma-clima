8801c8425a591874fc1cf7a373aa6b5b782a18c4
Lucas Tavares <lucastavarestt@gmail.com>
2024-10-14 12:07:21 -0300

    chore: other menu options temporarily removed

--- src/lib/menu-list.ts
+++ src/lib/menu-list.ts
@@ -29,18 +29,18 @@ type Group = {
 
 export function getMenuList(pathname: string): Group[] {
   return [
-    {
-      groupLabel: "",
-      menus: [
-        {
-          href: "/dashboard",
-          label: "Dashboard",
-          active: pathname.includes("/dashboard"),
-          icon: LayoutGrid,
-          submenus: []
-        }
-      ]
-    },
+    // {
+    //   groupLabel: "",
+    //   menus: [
+    //     {
+    //       href: "/dashboard",
+    //       label: "Dashboard",
+    //       active: pathname.includes("/dashboard"),
+    //       icon: LayoutGrid,
+    //       submenus: []
+    //     }
+    //   ]
+    // },
     {
       groupLabel: "Produtos",
       menus: [
@@ -85,88 +85,88 @@ export function getMenuList(pathname: string): Group[] {
              
           ]
         },
-        {
-          href: "",
-          label: "Radar",
-          active: pathname.includes("/radar"),
-          icon: Radar,
-          submenus: [
-            {
-              href: "",
-              label: "Mendanha",
-              active: pathname.includes("/radar/mendanha"),
-              submenus: [
-                {
-                  href: "/radar/mendanha/refletividade-horizontal",
-                  label: "Refletividade Horizontal",
-                  active: pathname === "/radar/mendanha/refletividade-horizontal"
-                },
-                {
-                  href: "/radar/mendanha/estimativa-de-chuva-atual-impa",
-                  label: "Estimativa de Chuva Atual (IMPA)",
-                  active: pathname === "/radar/mendanha/estimativa-de-chuva-atual-impa"
-                },
-              ]
-            }
-          ]
-        },
-        {
-          href: "",
-          label: "Pluviômetros",
-          active: pathname.includes("/pluviometros"),
-          icon: BarChartBigIcon,
-          submenus: [
-            {
-              href: "",
-              label: "AlertaRio",
-              active: pathname.includes("/pluviometros/alertario"),
-              submenus: [
-                {
-                  href: "/pluviometros/alertario/estimativa-de-chuva-atual",
-                  label: "Estimativa de Chuva Atual",
-                  active: pathname === "/pluviometros/alertario/estimativa-de-chuva-atual"
-                }
-              ]
-            }
-          ]
-        },
-        {
-          href: "",
-          label: "Previsão de Chuva",
-          active: pathname.includes("/previsao-de-chuva"),
-          icon: CloudRainWind,
-          submenus: [
-            {
-              href: "/previsao-de-chuva/1h2h3h/",
-              label: "1h, 2h, 3h",
-              active: pathname.includes("/previsao-de-chuva/1h2h3h"),
-              submenus: [
-                {
-                  href: "/previsao-de-chuva/1h2h3h/satelite-rio-now-cast",
-                  label: "Satélite (RioNowCast)",
-                  active: pathname === "/previsao-de-chuva/1h2h3h/satelite-rio-now-cast"
-                },
-                {
-                  href: "/previsao-de-chuva/1h2h3h/pluv-est-met-radar-rio-now-cast",
-                  label: "Estimativa de Chuva Atual",
-                  active: pathname === "/previsao-de-chuva/1h2h3h/pluv-est-met-radar-rio-now-cast"
-                }
-              ]
-            },
-            {
-              href: "/previsao-de-chuva",
-              label: "xy, yh, zh",
-              active: pathname === "/previsao-de-chuva",
-              submenus: [
-                {
-                  href: "/previsao-de-chuva/xyyhzh/IMPA",
-                  label: "IMPA",
-                  active: pathname === "/previsao-de-chuva/xyyhzh/IMPA"
-                }
-              ]
-            }
-          ]
-        }
+        // {
+        //   href: "",
+        //   label: "Radar",
+        //   active: pathname.includes("/radar"),
+        //   icon: Radar,
+        //   submenus: [
+        //     {
+        //       href: "",
+        //       label: "Mendanha",
+        //       active: pathname.includes("/radar/mendanha"),
+        //       submenus: [
+        //         {
+        //           href: "/radar/mendanha/refletividade-horizontal",
+        //           label: "Refletividade Horizontal",
+        //           active: pathname === "/radar/mendanha/refletividade-horizontal"
+        //         },
+        //         {
+        //           href: "/radar/mendanha/estimativa-de-chuva-atual-impa",
+        //           label: "Estimativa de Chuva Atual (IMPA)",
+        //           active: pathname === "/radar/mendanha/estimativa-de-chuva-atual-impa"
+        //         },
+        //       ]
+        //     }
+        //   ]
+        // },
+        // {
+        //   href: "",
+        //   label: "Pluviômetros",
+        //   active: pathname.includes("/pluviometros"),
+        //   icon: BarChartBigIcon,
+        //   submenus: [
+        //     {
+        //       href: "",
+        //       label: "AlertaRio",
+        //       active: pathname.includes("/pluviometros/alertario"),
+        //       submenus: [
+        //         {
+        //           href: "/pluviometros/alertario/estimativa-de-chuva-atual",
+        //           label: "Estimativa de Chuva Atual",
+        //           active: pathname === "/pluviometros/alertario/estimativa-de-chuva-atual"
+        //         }
+        //       ]
+        //     }
+        //   ]
+        // },
+        // {
+        //   href: "",
+        //   label: "Previsão de Chuva",
+        //   active: pathname.includes("/previsao-de-chuva"),
+        //   icon: CloudRainWind,
+        //   submenus: [
+        //     {
+        //       href: "/previsao-de-chuva/1h2h3h/",
+        //       label: "1h, 2h, 3h",
+        //       active: pathname.includes("/previsao-de-chuva/1h2h3h"),
+        //       submenus: [
+        //         {
+        //           href: "/previsao-de-chuva/1h2h3h/satelite-rio-now-cast",
+        //           label: "Satélite (RioNowCast)",
+        //           active: pathname === "/previsao-de-chuva/1h2h3h/satelite-rio-now-cast"
+        //         },
+        //         {
+        //           href: "/previsao-de-chuva/1h2h3h/pluv-est-met-radar-rio-now-cast",
+        //           label: "Estimativa de Chuva Atual",
+        //           active: pathname === "/previsao-de-chuva/1h2h3h/pluv-est-met-radar-rio-now-cast"
+        //         }
+        //       ]
+        //     },
+        //     {
+        //       href: "/previsao-de-chuva",
+        //       label: "xy, yh, zh",
+        //       active: pathname === "/previsao-de-chuva",
+        //       submenus: [
+        //         {
+        //           href: "/previsao-de-chuva/xyyhzh/IMPA",
+        //           label: "IMPA",
+        //           active: pathname === "/previsao-de-chuva/xyyhzh/IMPA"
+        //         }
+        //       ]
+        //     }
+        //   ]
+        // }
       ]
     }
   ];
