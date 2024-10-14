1b2d23637977f8959d9c94ba7d81c3973f97ecf0
Lucas Tavares <lucastavarestt@gmail.com>
2024-10-14 12:07:44 -0300

    fix: label from biggest to smallest

--- src/components/color-label.tsx
+++ src/components/color-label.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 
 interface ColorStop {
   color: string;
@@ -18,27 +18,31 @@ export default function ColorBar({
   height = 450,
   width = 15,
 }: ColorPaletteProps) {
-  const minValue = Math.min(...colorStops.map(item => item.value))
-  const maxValue = Math.max(...colorStops.map(item => item.value))
-  const range = maxValue - minValue
+  const minValue = Math.min(...colorStops.map(item => item.value));
+  const maxValue = Math.max(...colorStops.map(item => item.value));
+  const range = maxValue - minValue;
+
+  // Sort the colorStops in descending order
+  const sortedColorStops = [...colorStops].sort((a, b) => b.value - a.value);
 
   const getPosition = (value: number) => {
-    return ((value - minValue) / range) * 100
-  }
+    return ((maxValue - value) / range) * 100; // Reverse the calculation
+  };
 
   return (
     <div className="w-full max-w-xl mx-auto p-4 pt-10 rounded-lg">
       <div className="relative h-5 mb-8">
         <div className="flex justify-center absolute w-full text-center text-black font-bold -top-8">
-          {unit &&
-            <div className=" absolute bg-black text-white font-bold text-sm px-2 py-1 rounded-full">
+          {unit && (
+            <div className="absolute bg-black text-white font-bold text-sm px-2 py-1 rounded-full">
               {unit}
-            </div>}
+            </div>
+          )}
         </div>
         <svg width="100%" height="100%">
           <defs>
-            <linearGradient id="colorGradient" x1="0%" y1="0%" x2="100%" y2="0%">
-              {colorStops.map((item, index) => (
+            <linearGradient id="colorGradient" x1="0" y1="0%" x2="100%" y2="0%">
+              {sortedColorStops.map((item, index) => (
                 <stop
                   key={index}
                   offset={`${getPosition(item.value)}%`}
@@ -49,17 +53,17 @@ export default function ColorBar({
           </defs>
           <rect width="100%" height="100%" fill="url(#colorGradient)" rx="10" ry="10" />
         </svg>
-        {colorStops.map((item, index) => (
+        {sortedColorStops.map((item, index) => (
           <div
             key={index}
             className="absolute top-full mt-1 transform -translate-x-1/2 text-xs"
             style={{ left: `${getPosition(item.value)}%` }}
           >
-            <div className="h-2 w-px bg-gray-400 mb-1 mx-auto "></div>
+            <div className="h-2 w-px bg-gray-400 mb-1 mx-auto"></div>
             <div className="text-black font-bold text-sm text-right">{item.value}</div>
           </div>
         ))}
       </div>
     </div>
-  )
+  );
 }
