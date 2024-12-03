ce237cb213cae300996b294d58cb4292a4eaa22f
Lucas Tavares <lucastavarestt@gmail.com>
2024-12-03 15:54:34 -0300

    fix

--- src/components/admin-panel/sheet-menu.tsx
+++ src/components/admin-panel/sheet-menu.tsx
@@ -21,10 +21,16 @@ export function SheetMenu() {
       </SheetTrigger>
       <SheetContent className="sm:w-72 px-3 h-full flex flex-col" side="left">
         <SheetHeader>
-          <Link href="/dashboard" className="flex items-center gap-2 pb-2 pt-1">
-            <img src="https://cdn2.iconfinder.com/data/icons/weather-flat-14/64/weather02-512.png" alt="Weather Icon" className="w-6 h-6 mr-1" />
-            <SheetTitle className="font-bold text-lg">Plataforma Clima</SheetTitle>
-          </Link>
+          <Button
+            className="flex justify-center items-center pb-2 pt-1"
+            variant="link"
+            asChild
+          >
+            <Link href="/dashboard" className="flex items-center gap-2">
+              <img src="https://cdn2.iconfinder.com/data/icons/weather-flat-14/64/weather02-512.png" alt="Weather Icon" className="w-6 h-6 mr-1" />
+              <SheetTitle className="font-bold text-lg">Plataforma Clima</SheetTitle>
+            </Link>
+          </Button>
         </SheetHeader>
         <Menu isOpen />
       </SheetContent>
