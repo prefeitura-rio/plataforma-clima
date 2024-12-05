5c68da70cac4ab4a3b03fce0aa5ef3ba830a182a
Lucas Tavares <lucastavarestt@gmail.com>
2024-12-05 10:13:40 -0300

    fix

--- src/components/admin-panel/content-layout.tsx
+++ src/components/admin-panel/content-layout.tsx
@@ -2,13 +2,16 @@ import { Navbar } from "@/components/admin-panel/navbar";
 
 interface ContentLayoutProps {
   title: string;
+  view: string;
+  indice: string;
   children: React.ReactNode;
 }
 
-export function ContentLayout({ title, children }: ContentLayoutProps) {
+
+export function ContentLayout({ title, view, children, indice }: ContentLayoutProps) {
   return (
     <div>
-      <Navbar title={title} />
+      <Navbar title={title} view={view} indice={indice} />
       <div className="container pt-8 pb-8 px-4 sm:px-8">{children}</div>
     </div>
   );
