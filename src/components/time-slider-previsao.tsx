5440012e4891e2c811f2361bc6f09ad93ff91d0b
Lucas Tavares <lucastavarestt@gmail.com>
2024-12-17 17:37:14 -0300

    feat

--- src/components/time-slider-previsao.tsx
+++ src/components/time-slider-previsao.tsx
@@ -17,7 +17,7 @@ interface TimeSliderPrevisaoProps {
 }
 
 export function TimeSliderPrevisao({
-  name = "Produto",
+  name = "Carregando...",
   onTimeChange = () => { },
   sliderValue = 0,
   timestamps = [],
@@ -136,6 +136,7 @@ export function TimeSliderPrevisao({
   }, []);
 
   return (
+
     <div className="z-50 fixed bottom-2 w-[90%] sm:w-[50%] py-2 px-4 rounded-lg bg-gray-800 text-white">
       <div
         className={`absolute w-full bottom-full mb-2 ml-[-16px] transition-opacity duration-[1500ms] ${showImage && !isMobile ? "opacity-100" : "opacity-0"
