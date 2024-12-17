af8f72e4b218acb3da54c7fa67a8637caee20afc
Lucas Tavares <lucastavarestt@gmail.com>
2024-12-17 16:46:53 -0300

    fix

--- src/components/time-slider-previsao.tsx
+++ src/components/time-slider-previsao.tsx
@@ -136,7 +136,7 @@ export function TimeSliderPrevisao({
   }, []);
 
   return (
-    <div className="relative z-50 fixed bottom-2 w-[90%] sm:w-[50%] py-2 px-4 rounded-lg bg-gray-800 text-white">
+    <div className="z-50 fixed bottom-2 w-[90%] sm:w-[50%] py-2 px-4 rounded-lg bg-gray-800 text-white">
       <div
         className={`absolute w-full bottom-full mb-2 ml-[-16px] transition-opacity duration-[1500ms] ${showImage && !isMobile ? "opacity-100" : "opacity-0"
           }`}
