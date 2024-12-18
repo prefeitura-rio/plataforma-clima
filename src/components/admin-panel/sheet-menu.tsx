007c3cabf533b8d778bcc63a763ebcd5f50a5082
Lucas Tavares <lucastavarestt@gmail.com>
2024-12-18 10:39:53 -0300

    feat: new color label + mobile menu icon changed

--- src/components/admin-panel/sheet-menu.tsx
+++ src/components/admin-panel/sheet-menu.tsx
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import { MenuIcon, PanelsTopLeft } from "lucide-react";
+import { ChevronRight } from 'lucide-react';
 
 import { Menu } from "@/components/admin-panel/menu";
 import {
@@ -9,7 +9,6 @@ import {
   SheetTrigger,
   SheetTitle
 } from "@/components/ui/sheet";
-import { Button } from "../ui/button";
 
 interface SheetMenuProps {
   view?: string;
@@ -19,27 +18,22 @@ interface SheetMenuProps {
 export function SheetMenu({ view, indice }: SheetMenuProps) {
   return (
     <Sheet>
-      <SheetTrigger className="lg:hidden fixed top-2 left-4 z-50" >
-        <Button className="rounded-md w-7 h-7"
-          variant="outline" size="icon">
-          <MenuIcon size={20} />
-        </Button>
+      <SheetTrigger className="lg:hidden fixed top-24 left-0 z-50">
+        <div className="relative w-8 h-8">
+          <div className="absolute top-0 left-0 w-8 h-8 bg-gray-800 rounded-r-full"></div>
+          <ChevronRight className="absolute top-1/2 left-1.5 -translate-y-1/2 text-white" size={24} />
+        </div>
       </SheetTrigger>
       <SheetContent className="sm:w-72 pl-0 pr-1 h-full flex flex-col" side="left">
         <SheetHeader>
-          <Button
-            className="flex justify-center items-center pb-2 pt-1"
-            variant="link"
-            asChild
-          >
-            <Link href="/previsao-de-chuva/v1/mapa" className="flex items-center gap-2">
-              <img src="https://cdn2.iconfinder.com/data/icons/weather-flat-14/64/weather02-512.png" alt="Weather Icon" className="w-6 h-6 mr-1" />
-              <SheetTitle className="font-bold text-lg">Plataforma Clima</SheetTitle>
-            </Link>
-          </Button>
+          <Link href="/previsao-de-chuva/v1/mapa" className="flex items-center gap-2 pb-2 pt-1">
+            <img src="https://cdn2.iconfinder.com/data/icons/weather-flat-14/64/weather02-512.png" alt="Weather Icon" className="w-6 h-6 mr-1" />
+            <SheetTitle className="font-bold text-lg">Plataforma Clima</SheetTitle>
+          </Link>
         </SheetHeader>
         <Menu isOpen view={view} indice={indice} />
       </SheetContent>
     </Sheet>
   );
 }
+
