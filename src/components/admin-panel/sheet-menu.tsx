fd3e9c6c8bb42daa2542900728a9c7a22f7dd54c
Lucas Tavares <lucastavarestt@gmail.com>
2024-12-19 16:40:31 -0300

    style: back with the hamburguer menu button

--- src/components/admin-panel/sheet-menu.tsx
+++ src/components/admin-panel/sheet-menu.tsx
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import { ChevronRight } from 'lucide-react';
+import { ChevronRight, MenuIcon } from 'lucide-react';
 
 import { Menu } from "@/components/admin-panel/menu";
 import {
@@ -9,6 +9,7 @@ import {
   SheetTrigger,
   SheetTitle
 } from "@/components/ui/sheet";
+import { Button } from "../ui/button";
 
 interface SheetMenuProps {
   view?: string;
@@ -18,11 +19,12 @@ interface SheetMenuProps {
 export function SheetMenu({ view, indice }: SheetMenuProps) {
   return (
     <Sheet>
-      <SheetTrigger className="lg:hidden fixed top-24 left-0 z-50">
-        <div className="relative w-8 h-8">
-          <div className="absolute top-0 left-0 w-8 h-8 bg-gray-800 rounded-r-full"></div>
-          <ChevronRight className="absolute top-1/2 left-1.5 -translate-y-1/2 text-white" size={24} />
-        </div>
+      <SheetTrigger className="lg:hidden fixed top-24 left-4 z-50" >
+        <Button className="rounded-md w-7 h-7"
+          variant="outline" size="icon">
+          <MenuIcon size={20} />
+        </Button>
+
       </SheetTrigger>
       <SheetContent className="sm:w-72 pl-0 pr-1 h-full flex flex-col" side="left">
         <SheetHeader>
